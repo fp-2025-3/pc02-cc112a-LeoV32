4cd047ec1c0389546a8d623901cdfc80c58389d4
LeoV32 <leo.velasquez.r@uni.pe>
2026-02-11 16:15:59 -0500

    subiendo el ejercicio2 , y intercambiando el nombre del ejercicio 1 y 2 porque estaban al reves

--- semana5/lab/ejercicio1.cpp
+++ semana5/lab/ejercicio1.cpp
@@ -1,30 +1,36 @@
 #include <iostream>
-#include <cstring>
+#include <cmath>
 using namespace std;
 
-struct Estudiante{
-    string nombre;
-    int nota;
+struct Punto{
+    int x;
+    int y;
+    int z;
 };
 
+double distancia(Punto A1,Punto A2){
+
+    return sqrt(pow(A1.x-A2.x,2)+pow(A1.y-A2.y,2)+pow(A1.z-A2.z,2));
+}
+
+
+
 int main(){
-    int n;
-    cout<<"Ingrese el numero de estudiantes: ";
-    cin>>n;
-
-    Estudiante* e=new Estudiante[n];
-    int indice=0;
-
-    for(int i=0;i<n;i++){
-        cout<<"Ingrese el nombre del estudiante "<<i+1<<": ";
-        cin.ignore();
-        getline(cin,e[i].nombre);
-        cout<<"Ingrese la nota del estudiante "<<i+1<<": ";
-        cin>>e[i].nota;
-        if(e[i].nota>e[indice].nota) indice=i;
-    }
-
-    cout<<"El estudiante con la mayor nota es: ";
-    cout<<e[indice].nombre<<endl;
-    cout<<"Nota: "<<e[indice].nota<<endl;
+    Punto E1,E2;
+    cout<<"Ingrese las coordenadas del primer punto: "<<endl;
+    cout<<"X: ";
+    cin>>E1.x;
+    cout<<"Y: ";
+    cin>>E1.y;
+    cout<<"Z: ";
+    cin>>E1.z;
+    cout<<"Ingrese las coordenadas del segundo punto: "<<endl;
+    cout<<"X: ";
+    cin>>E2.x;
+    cout<<"Y: ";
+    cin>>E2.y;
+    cout<<"Z: ";
+    cin>>E2.z;
+    cout<<"La distancia entre los dos puntos es: "<<distancia(E1,E2)<<endl;
+    return 0;
 }
