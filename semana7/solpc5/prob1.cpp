b5c27ec87f91be8cc62916db3757942fe7c93dbb
LeoV32 <leo.velasquez.r@uni.pe>
2026-03-03 16:19:47 -0500

    Terminando de subir el ejercicio1 al solucionario

--- semana7/solpc5/prob1.cpp
+++ semana7/solpc5/prob1.cpp
@@ -23,8 +23,26 @@ int main(){
     float promedio;
     string condicion;
 
+    int cont=0;
+    double suma=0.0;
+    string maxNombre;
+    double maxNota=0.0;
+    string minNombre;
+    double minNota=20.0;
+
     while(archivoLectura>>codigo>>nombre>>notas[0]>>notas[1]>>notas[2]){
-        promedio=(notas[0]+notas[1]+notas[2])/3;
+        cont++;
+        promedio=(notas[0]+notas[1]+notas[2])/3.0;
+        suma+=promedio;
+
+        if(maxNota<promedio){
+            maxNota=promedio;
+            maxNombre=nombre;
+        }else if(minNota>promedio){
+            minNota=promedio;
+            minNombre=nombre;
+        }
+
 
         if(notas[0]<5 || notas[1]<5 || notas[2]<5) condicion="DESAPROBADO POR REGLA ACADEMICA";
         else if(promedio>=10) condicion="APROBADO";
@@ -32,6 +50,10 @@ int main(){
 
         archivoEscritura<<codigo<<"\t"<<nombre<<"\t\t"<<promedio<<"\t\t"<<condicion<<endl;
     }
+    archivoEscritura<<endl<<"Total de estudiantes: "<<cont<<endl;
+    archivoEscritura<<"Promedio general: "<<suma/cont<<endl;
+    archivoEscritura<<"Estudiante con mayor promedio: "<<maxNombre<<"\t"<<maxNota<<endl;
+    archivoEscritura<<"Estudiante con menor promedio: "<<minNombre<<"\t"<<minNota<<endl;
 
     archivoEscritura.close();
     archivoLectura.close();
